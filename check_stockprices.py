c3965f9b06afa5ddae0db999f3031a7119367886
Rickk Barbosa <rickkbarbosa@users.noreply.github.com>
2022-02-24 21:20:47 -0300

    Update check_stockprices.py
    adding User agent on script

--- check_stockprices.py
+++ check_stockprices.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
 import sys
@@ -11,8 +11,11 @@ def getValor(acao):
 
     acao = acao.upper()
     url = "http://br.advfn.com/p.php?pid=qkquote&symbol=" + acao
-    r = requests.get(url)
-    
+    headers = {}
+    headers["User-Agent"] = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"
+
+    r = requests.get(url, headers=headers)
+
     soup = BeautifulSoup(r.text, 'html.parser')
     span = soup.find("span", id="quoteElementPiece1")
 
